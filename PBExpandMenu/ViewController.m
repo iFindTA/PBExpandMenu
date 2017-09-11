804e644ee38d7e41dd00858add1cd14fed621eab
nanhujiaju <hujiaju@hzflk.com>
2017-09-11 09:06:46 +0800

    Add controller

--- PBExpandMenu/ViewController.m
+++ PBExpandMenu/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "PBExpandMenu.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor colorWithRed:1 green:0.58 blue:0.27 alpha:1];
+    
+    UIImage *startImage = [UIImage imageNamed:@"start"];
+    UIImage *image1 = [UIImage imageNamed:@"icon-twitter"];
+    UIImage *image2 = [UIImage imageNamed:@"icon-email"];
+    UIImage *image3 = [UIImage imageNamed:@"icon-facebook"];
+    NSArray *images = @[image1, image2, image3];
+    PBExpandMenu *menu = [[PBExpandMenu alloc] initWithCenterPoint:CGPointMake(CGRectGetWidth(self.view.frame) / 2, 320)
+                                                         startImage:startImage
+                                                      submenuImages:images];
+    menu.damping = 0.3;
+    menu.ballLength = 85;
+    [self.view addSubview:menu];
+    
 }
 
 
---
+++ PBMenu/PBMenu.h
@@ -0,0 +1,12 @@
+//
+//  PBMenu.h
+//  PBMenu
+//
+//  Created by nanhujiaju on 2017/9/8.
+//  Copyright © 2017年 nanhujiaju. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PBExpandMenu.h"
+
